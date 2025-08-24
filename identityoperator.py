b84d421df4aaaa5e8cfbdca0ef7c638418cef15c
ProgrammerBoyAditya <utrejaaditya@gmail.com>
2025-08-24 09:05:50 +0530

    Add files via upload

---
+++ identityoperator.py
@@ -0,0 +1,11 @@
+x = 7
+if (type(x) is int ):
+    print("x has an int value")
+else:
+    print("x does not have an int value")
+
+y = 7
+if (x is y ):
+    print("X and Y have the same identity")
+else :
+    print("X and Y do not have the same identity")
